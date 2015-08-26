36a5771867bb940f69f80347c28c489d761c284c
Dmitriy Isaev <dartkorvin@yandex.ru>
2015-08-26 15:21:59 +0300

    * fixes

--- app/main.go
+++ app/main.go
@@ -11,21 +11,64 @@ type Index struct {
 
 // Panics if error is non-nil
 var indexTemplate = template.Must(template.New("index").ParseFiles("tmpl/index.html"))
-var autobioTpl = template.Must(template.New("autobiografia").ParseFiles("tmpl/autobiografia.html"))
+var autobioTpl = template.Must(template.New("autobiografiya").ParseFiles("tmpl/autobiografiya.html"))
 var dojosTpl = template.Must(template.New("drugie-dojo").ParseFiles("tmpl/drugie-dojo.html"))
 var partnersTpl = template.Must(template.New("partnery").ParseFiles("tmpl/partnery.html"))
-var photoTpl = template.Must(template.New("photo").ParseFiles("tmpl/partnery.html"))
+var photoTpl = template.Must(template.New("photo").ParseFiles("tmpl/photo.html"))
+var videoTpl = template.Must(template.New("video").ParseFiles("tmpl/video.html"))
 
 func init() {
+	http.HandleFunc("/autobiografiya", autobiografia)
+	http.HandleFunc("/drugie-dojo", dojos)
+	http.HandleFunc("/partnery", partnery)
+	http.HandleFunc("/photo", photo)
+	http.HandleFunc("/video", video)
 	http.HandleFunc("/", root)
 }
 
 func root(w http.ResponseWriter, r *http.Request) {
-	index := Index{
-		Title: "hlilje",
-	}
 
-	err := indexTemplate.Execute(w, index)
+	err := indexTemplate.Execute(w, new(Index))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func dojos(w http.ResponseWriter, r *http.Request) {
+
+	err := dojosTpl.Execute(w, new(Index))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func autobiografia(w http.ResponseWriter, r *http.Request) {
+
+	err := autobioTpl.Execute(w, new(Index))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func partnery(w http.ResponseWriter, r *http.Request) {
+
+	err := partnersTpl.Execute(w, new(Index))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func photo(w http.ResponseWriter, r *http.Request) {
+
+	err := photoTpl.Execute(w, new(Index))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func video(w http.ResponseWriter, r *http.Request) {
+
+	err := videoTpl.Execute(w, new(Index))
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
